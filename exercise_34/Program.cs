ce8b6269221dd7b8bd38119d75e5ec3dbcc0870d
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-11 11:13:40 +0300

    exercise 34 had been accomplished

---
+++ exercise_34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] RandomArray()
+{
+    int[] Array = new int[new Random().Next(3,11)];
+    
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(99,1000);
+    }
+    return Array;
+}
+
+int EvenCalculus(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Amount of even numbers in array is: {count}");
+    return count;
+}
+
+EvenCalculus(RandomArray());
